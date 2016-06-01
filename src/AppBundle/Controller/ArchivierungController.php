66fcfb46e41f0259ea36418606537b72083f39b7
moeb0797 <morice.ebbertz@fh-duesseldorf.de>
2016-06-01 20:29:15 +0200

    Neu/Edit Formulare hinzugefügt

--- src/AppBundle/Controller/ArchivierungController.php
+++ src/AppBundle/Controller/ArchivierungController.php
@@ -6,10 +6,92 @@ use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Method;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use AppBundle\Entity\Archivierung;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\Form\Extension\Core\Type\SubmitType;
+use Symfony\Bridge\Doctrine\Form\Type\EntityType;
 
 class ArchivierungController extends Controller {
+    /**
+     * @Route("/archivierung/neu",
+     *     name="_neuArchivierung"
+     * )
+     */
+    public function neuArchivierungAction(Request $request) {
+        // Neue Archivierung
+        $archivierung = new Archivierung();
 
-    /////// DetailView
+        // Formular erstellen
+        $form = $this->getArchivierungForm($archivierung);
+        $form->handleRequest($request);
+
+        // Wurde das Formular abgesendet?
+        if ($form->isSubmitted()) {
+
+            // Ist das Forumlar valide?
+            if($form->isValid()) {
+                // Archivierung in der DB anlegen
+                $this->saveArchivierung($archivierung);
+
+                // Benachrichtigung: gespeichert
+                $this->addFlash(
+                    'success',
+                    'Ihre Eingaben wurden erfolgreich gespeichert.'
+                );
+            }
+            // Formular enthält Fehler
+            else {
+
+            }
+        }
+
+        return $this->render(
+            'Archivierung/form.html.twig',
+            array(
+                'form' => $form->createView()
+            )
+        );
+    }
+
+    /**
+     * @Route("/archivierung/bearbeiten/{archivierungId}",
+     *     name="_editArchivierung"
+     * )
+     */
+    public function editArchivierungAction(Request $request, $archivierungId) {
+        $archivierung = $this->getArchivierung($archivierungId);
+
+        // Formular erstellen
+        $form = $this->getArchivierungForm($archivierung);
+        $form->handleRequest($request);
+
+        // Wurde das Formular abgesendet?
+        if ($form->isSubmitted()) {
+
+            // Ist das Forumlar valide?
+            if($form->isValid()) {
+                $this->saveArchivierung($archivierung);
+
+                // Benachrichtigung: gespeichert
+                $this->addFlash(
+                    'success',
+                    'Ihre Eingaben wurden erfolgreich gespeichert.'
+                );
+            }
+            // Formular enthält Fehler
+            else {
+
+            }
+        }
+
+        return $this->render(
+            'archivierung/form-edit.html.twig',
+            array(
+                'form' => $form->createView()
+            )
+        );
+    }
+
     /**
      * @Route("/archivierung/detail/{archivId}",
      *     name="_detailView",
@@ -57,42 +139,74 @@ class ArchivierungController extends Controller {
      * @return \Symfony\Component\HttpFoundation\Response
      */
     public function detailViewFileAction($path, $erstellerId) {
-
         // checken ob Zugriff auf Path erlaubt
         // TODO wenn userId == erstellerId oder Admin oder Prof, und wenn referer = detailseite.
         // TODO PDF returnen
-
-
-
-
     }
 
-
-
-    /////// Edit und Create
-
-    // EBBI... <3
-
-
-
-
-
-
-
-    /////// Private Methoden
-    private function getArchivierung($archivId) {
+    private function getArchivierung($archivierungId) {
         $archivierung = $this->getDoctrine()
             ->getRepository('AppBundle:Archivierung')
-            ->find($archivId);
+            ->find($archivierungId);
 
-        // Keine Archivierung gefunden
+        // Keinen Archivierung gefunden
         if (!$archivierung) {
             throw $this->createNotFoundException(
-                'Keine Archivierung mit der id ' . $archivId . 'gefunden!'
+                'Keine Archivierung mit der id ' . $archivierungId . 'gefunden!'
             );
         }
 
         return $archivierung;
+    }
+
+    private function getArchivierungForm($archivierung) {
+        // Erzeugt ein Formular für das Objekt Archivierung
+        $form = $this->createFormBuilder($archivierung)
+            ->add('titel', TextType::class)
+            ->add('beschreibung', TextType::class)
+            ->add('fachbereich', EntityType::class, array(
+                // query choices from this entity
+                'class' => 'AppBundle:Fachbereich',
+                // use the User.username property as the visible option string
+                'choice_label' => 'bezeichnung',
+                'placeholder' => 'Bitte wählen',
+                // used to render a select box, check boxes or radios
+                'multiple' => false,
+                'expanded' => false,
+            ))
+            ->add('studiengang', EntityType::class, array(
+                // query choices from this entity
+                'class' => 'AppBundle:studiengang',
+                // use the User.username property as the visible option string
+                'choice_label' => 'bezeichnung',
+                'placeholder' => 'Bitte wählen',
+                // used to render a select box, check boxes or radios
+                'multiple' => false,
+                'expanded' => false,
+            ))
+            ->add('kategorie', EntityType::class, array(
+                // query choices from this entity
+                'class' => 'AppBundle:ArchivKategorie',
+                // use the User.username property as the visible option string
+                'choice_label' => 'bezeichnung',
+                'placeholder' => 'Bitte wählen',
+                // used to render a select box, check boxes or radios
+                'multiple' => false,
+                'expanded' => false,
+            ))
+            ->add('speichern', SubmitType::class)
+            ->getForm();
+        return $form;
+    }
+
+    private function saveArchivierung($archivierung) {
+        $entityManager = $this->getDoctrine()->getManager();
+
+        // tells Doctrine you want to (eventually) save the Product (no queries yet)
+        $entityManager->persist($archivierung);
+
+        // actually executes the queries (i.e. the INSERT query)
+        $entityManager->flush();
     }
 
 
