67ee80fa4a65be12fe66e395abe40eca23b1409c
David <nowottnik@googlemail.com>
2016-06-07 14:10:34 +0200

    Such-Controller Style und JS Anpassungen

--- web/assets/js/scripts.js
+++ web/assets/js/scripts.js
@@ -53,8 +53,8 @@ $(function() {
 
     } // if($('body.suche'))
 
-    // Datepicker binden - Zeitraum
-    $('.datepicker').daterangepicker({
+    // Datepicker Options & Lokalisation
+    var datepickerOptions = {
         'showDropdowns': true,
         'autoUpdateInput': false,
         'autoApply': true,
@@ -91,96 +91,33 @@ $(function() {
             ],
             "firstDay": 1
         }
-    }).on('apply.daterangepicker', function(ev, picker) {
+    };
+
+    // Datepicker binden - Zeitraum
+    $('.datepicker').daterangepicker(
+        datepickerOptions
+    ).on('apply.daterangepicker', function(ev, picker) {
         $(this).val(picker.startDate.format('DD.MM.YYYY') + ' bis ' + picker.endDate.format('DD.MM.YYYY'));
     }).on('cancel.daterangepicker', function(ev, picker) {
         $(this).val('');
     });
 
-    $('.datepicker-right').daterangepicker({
-        'opens': 'left',
-        'showDropdowns': true,
-        'autoUpdateInput': false,
-        'autoApply': true,
-        'locale': {
-            'format': 'DD.MM.YYYY',
-            "separator": " - ",
-            "applyLabel": "Übernehmen",
-            "cancelLabel": "Abbrechen",
-            "fromLabel": "Von",
-            "toLabel": "Bis",
-            "customRangeLabel": "Custom",
-            "daysOfWeek": [
-                "So",
-                "Mo",
-                "Di",
-                "Mi",
-                "Do",
-                "Fr",
-                "Sa"
-            ],
-            "monthNames": [
-                "Januar",
-                "Februar",
-                "März",
-                "April",
-                "Mai",
-                "Juni",
-                "Juli",
-                "August",
-                "September",
-                "Oktober",
-                "November",
-                "Dezember"
-            ],
-            "firstDay": 1
-        }
-    }).on('apply.daterangepicker', function(ev, picker) {
+    // Datepicker binden - Zeitraum rechtsbündig
+    datepickerOptions.opens = 'left';
+    $('.datepicker-right').daterangepicker(
+        datepickerOptions
+    ).on('apply.daterangepicker', function(ev, picker) {
         $(this).val(picker.startDate.format('DD.MM.YYYY') + ' bis ' + picker.endDate.format('DD.MM.YYYY'));
     }).on('cancel.daterangepicker', function(ev, picker) {
         $(this).val('');
     });
 
     // Datepicker binden - Einfaches Datum
-    $('.datepicker-single').daterangepicker({
-        'singleDatePicker': true,
-        'showDropdowns': true,
-        'autoUpdateInput': false,
-        'autoApply': true,
-        'locale': {
-            'format': 'DD.MM.YYYY',
-            "separator": " - ",
-            "applyLabel": "Übernehmen",
-            "cancelLabel": "Abbrechen",
-            "fromLabel": "Von",
-            "toLabel": "Bis",
-            "customRangeLabel": "Custom",
-            "daysOfWeek": [
-                "So",
-                "Mo",
-                "Di",
-                "Mi",
-                "Do",
-                "Fr",
-                "Sa"
-            ],
-            "monthNames": [
-                "Januar",
-                "Februar",
-                "März",
-                "April",
-                "Mai",
-                "Juni",
-                "Juli",
-                "August",
-                "September",
-                "Oktober",
-                "November",
-                "Dezember"
-            ],
-            "firstDay": 1
-        }
-    }).on('apply.daterangepicker', function(ev, picker) {
+    datepickerOptions.opens = 'right';
+    datepickerOptions.singleDatePicker = true;
+    $('.datepicker-single').daterangepicker(
+        datepickerOptions
+    ).on('apply.daterangepicker', function(ev, picker) {
         $(this).val(picker.startDate.format('DD.MM.YYYY'));
     }).on('cancel.daterangepicker', function(ev, picker) {
         $(this).val('');
