00dd22a4aadc043e88e36cc4c16c287dc6e6b6f1
David <nowottnik@googlemail.com>
2016-06-23 15:09:57 +0200

    Dynamisches eiditieren von Keywords

--- src/AppBundle/Form/Type/KeywordType.php
+++ src/AppBundle/Form/Type/KeywordType.php
@@ -4,15 +4,12 @@ namespace AppBundle\Form\Type;
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\Form\Extension\Core\Type\TextType;
-use Symfony\Component\Form\Extension\Core\Type\HiddenType;
 use Symfony\Component\OptionsResolver\OptionsResolver;
 
 class KeywordType extends AbstractType
 {
     public function buildForm(FormBuilderInterface $builder, array $options) {
-        $builder
-            ->add('keywordId', HiddenType::class)
-            ->add('keyword', TextType::class);
+        $builder->add('keyword', TextType::class);
     }
 
     public function configureOptions(OptionsResolver $resolver) {
