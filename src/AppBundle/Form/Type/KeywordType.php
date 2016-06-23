a78a667e9ca634eeed56711f0b78a9570b5b4f31
moeb0797 <morice.ebbertz@fh-duesseldorf.de>
2016-06-23 12:32:06 +0200

    Neu/Edit Formular

---
+++ src/AppBundle/Form/Type/KeywordType.php
@@ -0,0 +1,23 @@
+<?php
+namespace AppBundle\Form\Type;
+
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\Form\Extension\Core\Type\HiddenType;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+
+class KeywordType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options) {
+        $builder
+            ->add('keywordId', HiddenType::class)
+            ->add('keyword', TextType::class);
+    }
+
+    public function configureOptions(OptionsResolver $resolver) {
+        $resolver->setDefaults(array(
+            'data_class' => 'AppBundle\Entity\Keyword',
+        ));
+    }
+}
